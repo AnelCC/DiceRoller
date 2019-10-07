a2b6cad11feb666543d5dc5723d272d0cd28be5f
Anel CC <anelcc05@gmail.com>
2019-10-06 22:04:33 -0400

    implement lateinit and get the dice number

--- app/src/main/java/com/anelcc/diceroller/MainActivity.kt
+++ app/src/main/java/com/anelcc/diceroller/MainActivity.kt
@@ -5,19 +5,39 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
 
         rollButton.setOnClickListener(){
             rollDice()
         }
+        diceImage.setOnClickListener(){
+            getDiceImageNumber()
+        }
+
+    }
+
+    private fun getDiceImageNumber() {
+        val number = when (diceImage.id) {
+            R.drawable.dice_1 -> 1
+            R.drawable.dice_2 -> 2
+            R.drawable.dice_3 -> 3
+            R.drawable.dice_4 -> 4
+            R.drawable.dice_5 -> 5
+            else -> 6
+        }
+
+        Toast.makeText(this, "Dice number is "+number,Toast.LENGTH_LONG).show()
     }
 
     private fun rollDice() {
@@ -33,7 +53,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage : ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
