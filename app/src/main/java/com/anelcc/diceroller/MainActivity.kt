cbe8eb667b7dde272ebeadb9524699850d7d9196
Anel CC <anelcc05@gmail.com>
2019-10-06 22:08:58 -0400

    remove extra code but keep the lateinit or global variable

--- app/src/main/java/com/anelcc/diceroller/MainActivity.kt
+++ app/src/main/java/com/anelcc/diceroller/MainActivity.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,23 +19,6 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener(){
             rollDice()
         }
-        diceImage.setOnClickListener(){
-            getDiceImageNumber()
-        }
-
-    }
-
-    private fun getDiceImageNumber() {
-        val number = when (diceImage.id) {
-            R.drawable.dice_1 -> 1
-            R.drawable.dice_2 -> 2
-            R.drawable.dice_3 -> 3
-            R.drawable.dice_4 -> 4
-            R.drawable.dice_5 -> 5
-            else -> 6
-        }
-
-        Toast.makeText(this, "Dice number is "+number,Toast.LENGTH_LONG).show()
     }
 
     private fun rollDice() {
